946c1fef902a5918f7a7ae5109cd4a7855a81f54
alaminrahman <sumonrahman3600@gmail.com>
2023-09-03 14:34:56 +0600

    Json, form, header, cookie  Data received

--- practice/m-13/app/api/product/route.js
+++ practice/m-13/app/api/product/route.js
@@ -1,9 +1,29 @@
 import { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 import { headers } from "next/headers";
-import { redirect } from "next/navigation";
 
 
-export async function GET(req, res){
-  redirect('/');
+export async function POST(req, res){
+  //encoded data
+  // let reqBody = await req.formData();
+  // let name = reqBody.get('name');
+
+  // let headerList = headers();
+  // let authToken = headerList.get("Auth-Token");
+
+  //Cookies
+  let csrf_token = req.cookies.get("csrf_token");
+  let token = req.cookies.get("csrf_token");
+
+    return NextResponse.json({
+        data: csrf_token,
+        token : token
+    }, { 
+      status: 200, 
+      headers: {
+        token: "tfasdfsd",
+        'Set-Cookie': 'token=cookietoken, path=/'
+      },
+      
+    });
 }
