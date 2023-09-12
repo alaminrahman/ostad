3666be3b87627bfb4b9c27002269d23a9acc5b3a
alaminrahman <sumonrahman3600@gmail.com>
2023-09-12 07:43:29 +0600

    Middleware working, set header from middleware and get from controller

--- practice/m-13/src/app/api/product/route.js
+++ practice/m-13/src/app/api/product/route.js
@@ -1,11 +1,17 @@
+import { headers } from "next/headers";
 import { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
 
+
 export async function GET(req, res){
+  
+  const list = headers();
+  const user = list.get('user')
+
   return NextResponse.json({
-    name: 'test'
-    
-  }, { status: 200});
+    msg: 'I am msg',
+    name: user,
+  }, { status: 200 });
 
 }
--- practice/m-13/src/middleware.js
+++ practice/m-13/src/middleware.js
@@ -1,11 +1,21 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export const config = {
-    matcher : '/'
-} 
-
 export function middleware(req, res){
-    console.log(req.nextUrl.pathname);
-    console.log("Im middleware");
-    console.log(" ");
+    if(req.nextUrl.pathname.startsWith('/api/product')){
+        const reqHeaders = new Headers(req.headers);
+        const token = reqHeaders.get('token');
+
+        console.log(token)
+    
+        if(token == '123-XYZ'){
+            reqHeaders.set('user', 'alamin')
+            reqHeaders.set('email', 'alamin@gmail.com')
+
+            return NextResponse.next({
+                request: { headers: reqHeaders }
+            });
+        }else{
+            return NextResponse.json({}, { status: 401 });
+        }
+    }
 }
